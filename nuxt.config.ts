8d399ec797047bdb0682ab1b0ee69ce3dea45822
sianglife <sianglifen@gmail.com>
2026-03-21 19:08:03 +0800

    feat: metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,46 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxtjs/tailwindcss", "@nuxt/scripts"],
 	css: ["~/assets/css/main.css"],
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: "zh-TW",
+			},
+			title: "圖上字",
+			meta: [
+				{
+					charset: "utf-8",
+				},
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1",
+				},
+				{
+					name: "description",
+					content: "圖上字，輕鬆幫證件標註文字的幫手",
+				},
+				{
+					name: "keywords",
+					content: "圖片上字, 證件上字, 上字, 文字工具",
+				},
+				{
+					name: "language",
+					content: "zh-TW",
+				},
+				{
+					httpEquiv: "X-UA-Compatible",
+					content: "ie=edge",
+				},
+			],
+			link: [
+				{
+					rel: "icon",
+					type: "image/svg+xml",
+					href: "/favicon.svg",
+				},
+			],
+		},
+	},
 	$production: {
 		scripts: {
 			registry: {
